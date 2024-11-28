d41f05c44a6fffd83b62014ed0c4f5c0cda24f68
Deepu Cyriac <deepucvk123@gmail.com>
2024-11-28 22:30:55 +0530

    Fix for 6th test case - custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -7,7 +7,14 @@ class stringCalculator {
         //regex for newline and comma
         let delimiters = /,|\n/;
 
-        //split the string based on , and \n and return sum
+        //below code finds the specified delimiter if present and use it to separate the given numbers
+        if(numbers.startsWith("//")) {
+            const lastIndex = numbers.indexOf("\n");
+            delimiters = new RegExp(numbers.slice(2, lastIndex));
+            numbers = numbers.slice(lastIndex + 1);
+        }
+
+        //split the string based on specified delimiters or on , and \n and return sum
         const numberArray = numbers.split(delimiters).map(num => parseInt(num));
         return numberArray.reduce((sum, num) => sum + num, 0);
     }
