b5e06cc32a8b139c3753851eb32ae3e84e9e2431
Deepu Cyriac <deepucvk123@gmail.com>
2024-11-28 23:35:05 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ A String Calculator using Test Driven Development
 1. Clone the repo
 2. Run "npm i" in project root folder
 
+- To run with custom input:
+1. Assign your custom input to "input" variable in index.js
+2. Run "npm start" in project root folder
 
 - To run tests:
 1. Run "npm test" in project root folder
--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const StringCalculator = require('./stringCalculator');
 
 const calculator = new StringCalculator();
 
+//assign custom input here
 let input = "1,-2,3,-4";
 
 try {
+
     const result = calculator.add(input);
     console.log(result);
+
 } catch (error) {
+
     console.error(error.message);
+    
 }
