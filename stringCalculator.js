c8111dfe8ac4db6c80360250c7816c50abb4f684
Deepu Cyriac <deepucvk123@gmail.com>
2024-11-28 22:16:02 +0530

    Fix for 5th test case

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,8 +4,11 @@ class stringCalculator {
         //if input string is empty return 0
         if(!numbers) return 0;
 
-        //below code will work for 1, 2 and unknown numbers test cases
-        const numberArray = numbers.split(',').map(num => parseInt(num));
+        //regex for newline and comma
+        let delimiters = /,|\n/;
+
+        //split the string based on , and \n and return sum
+        const numberArray = numbers.split(delimiters).map(num => parseInt(num));
         return numberArray.reduce((sum, num) => sum + num, 0);
     }
 }
