60d81885c4f2edcf239c53dd18c32b13a5c13836
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-02 21:37:24 +0530

    Code refactoring - Removed some comments & updated variable names

--- stringCalculator/stringCalculator.js
+++ stringCalculator/stringCalculator.js
@@ -1,35 +1,29 @@
 class stringCalculator {
     add(numbers) {
 
-        //if input string is empty return 0
         if(!numbers) return 0;
 
-        //regex for newline and comma
         let delimiters = /,|\n/;
 
         if(this.isCustomDelimiter(numbers)) {
             const lastIndex = numbers.indexOf("\n");
 
-            const delimPart = numbers.slice(2, lastIndex);
+            const delimiterPart = numbers.slice(2, lastIndex);
 
-            const customDelim = delimPart.match(/\[.*?\]/g);
+            const multipleDelimiter = delimiterPart.match(/\[.*?\]/g);
             
-            if (customDelim) {
-                //extract delimiters, escape special regex chars, and join into one regex
+            if (multipleDelimiter) {
                 delimiters = new RegExp(
-                    customDelim.map(d => d.slice(1, -1).replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|")
+                    multipleDelimiter.map(d => d.slice(1, -1).replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|")
                 );
             } else {
-                //single character custom delimiter
-                delimiters = new RegExp(delimPart.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+                delimiters = new RegExp(delimiterPart.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
             }
             numbers = numbers.slice(lastIndex + 1);
         }
 
-        //split the string based on specified delimiters or on , and \n and filter numbers greater than 1000 and then return sum
         const numberArray = numbers.split(delimiters).map(num => parseInt(num)).filter(num => num <= 1000);
 
-        //check for negative number
         const negativeNumbers = numberArray.filter(num => num < 0);
         if(negativeNumbers.length) {
             throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(", ")}`);
--- stringCalculator/stringCalculator.test.js
+++ stringCalculator/stringCalculator.test.js
@@ -7,57 +7,46 @@ describe('stringCalculator', () => {
         calculator = new stringCalculator();
     });
 
-    //test to check if empty string input function will return value 0
     test('should return 0 if string is empty', () => {
         expect(calculator.add("")).toBe(0);
     });
 
-    //test to check if the number is returned if only one number is provided as input
     test('should return the number if one number is provided', () => {
         expect(calculator.add("3")).toBe(3);
     });
 
-    //test to check if sum is returned for two comma-separated numbers
     test('should return sum of two numbers - comma separated', () => {
         expect(calculator.add("2,3")).toBe(5);
     });
 
-    //test to check sum for unknown amount of numbers
     test('should handle an unknown amount of numbers', () => {
         expect(calculator.add("1,2,3,4,5,6")).toBe(21);
     });
 
-    //test to check if code handles new lines between numbers
     test('should handle new lines between numbers', () => {
         expect(calculator.add("1\n2,3")).toBe(6);
     });
 
-    //test to check if code supports different delimiters
     test('should provide support for different delimiters - specified at begining of input', () => {
         expect(calculator.add("//;\n1;2")).toBe(3);
     });
 
-    //test to check if code handles negative numbers
     test('should throw an exception if negative numbers are present', () => {
         expect(() => calculator.add("1,-2,3,-4")).toThrow("Negative numbers not allowed: -2, -4");
     });
 
-    //test to check if code handles numbers greater than 1000
     test('if numbers greater than 1000, ignore them', () => {
         expect(calculator.add("2,1001")).toBe(2);
     })
 
-    //test to check if code handles multi character delimiter
     test('should handle a custom delimiter of any length', () => {
         expect(calculator.add("//[***]\n1***2***3")).toBe(6);
     });
 
-    //test to check if code handles multiple single-character delimiters
     test('should handle multiple single-character delimiters', () => {
         expect(calculator.add("//[*][%]\n1*2%3")).toBe(6);
     });
 
-    //test to check if code handles multiple multi-character delimiters
     test('should handle multiple multi-character delimiters', () => {
         expect(calculator.add("//[***][%%]\n1***2%%3")).toBe(6);
     });
